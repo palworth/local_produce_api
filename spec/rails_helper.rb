8a2622580998888cca86909ed64a0d9631768b37
Pierce Alworth <pbalworth@gmail.com>
2022-03-05 16:41:19 -0700

    fix typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-]require 'vcr'
+require 'vcr'
 require 'webmock'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
